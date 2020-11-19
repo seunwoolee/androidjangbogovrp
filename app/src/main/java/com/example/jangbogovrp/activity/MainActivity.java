0398a78b27bb512d28b160a42e465b29479967dd
seunwoo <leemoney93@naver.com>
2020-11-19 15:13:37 +0900

    drawer 적용

--- app/src/main/java/com/example/jangbogovrp/activity/MainActivity.java
+++ app/src/main/java/com/example/jangbogovrp/activity/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.jangbogovrp.activity;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.widget.NestedScrollView;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -14,6 +16,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.jangbogovrp.R;
@@ -25,6 +30,7 @@ import com.example.jangbogovrp.http.RetrofitClient;
 import com.example.jangbogovrp.model.RouteD;
 import com.example.jangbogovrp.model.User;
 import com.example.jangbogovrp.utils.Tools;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 import java.io.IOException;
@@ -39,6 +45,10 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "MainActivity";
     private TabLayout tab_layout;
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private View navigation_header;
+
     private Realm mRealm;
     private long mPressedTime;
     private ArrayList mRouteDS;
@@ -47,13 +57,11 @@ public class MainActivity extends AppCompatActivity {
         public void onResponse(Call<List<RouteD>> call, Response<List<RouteD>> response) {
             if (response.isSuccessful()) {
                 mRouteDS = (ArrayList) response.body();
-
                 if (isLogin()) {
                     initMapFragment();
                 } else {
                     goToLogin();
                 }
-
             }
         }
 
@@ -62,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
     @Override
     public void onBackPressed() {
         if (mPressedTime == 0) {
@@ -86,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initToolbar();
         initComponent();
+        initDrawerMenu();
         mRealm = Tools.initRealm(this);
         User user = mRealm.where(User.class).findFirst();
         HttpService httpService = RetrofitClient.getHttpService(user.key);
@@ -104,8 +114,9 @@ public class MainActivity extends AppCompatActivity {
         return user != null;
     }
 
+
     private void initToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_menu);
         toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.light_blue_500), PorterDuff.Mode.SRC_ATOP);
         setSupportActionBar(toolbar);
@@ -114,6 +125,40 @@ public class MainActivity extends AppCompatActivity {
         Tools.setSystemBarColor(this, R.color.grey_5);
         Tools.setSystemBarLight(this);
     }
+
+    private void initDrawerMenu() {
+        final NavigationView nav_view = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            public void onDrawerOpened(View drawerView) {
+//                updateCounter(nav_view);
+                super.onDrawerOpened(drawerView);
+            }
+        };
+//        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(final MenuItem item) {
+                int item_id = item.getItemId();
+                if (item_id == R.id.action_notifications) {
+
+                } else if (item_id == R.id.action_rate) {
+//                    Tools.rateAction(MainMenu.this);
+                } else if (item_id == R.id.action_about) {
+//                    showDialogAbout();
+                }
+                return true;
+            }
+        });
+
+        // navigation header
+        navigation_header = nav_view.getHeaderView(0);
+
+        TextView tv_new_version = (TextView) navigation_header.findViewById(R.id.tv_new_version);
+        tv_new_version.setVisibility(View.GONE);
+    }
+
 
     private void initComponent() {
         tab_layout = (TabLayout) findViewById(R.id.tab_layout);
@@ -147,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_logout, menu);
-        Tools.changeMenuIconColor(menu, getResources().getColor(R.color.light_blue_500));
+        Tools.changeMenuIconColor(menu, getResources().getColor(R.color.grey_90));
         return true;
     }
 
